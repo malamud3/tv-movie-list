dcd64adc7ae4e98e60693dc5f3bd2dbed2c06b08
amir <malamud95@gmail.com>
2025-03-01 16:56:26 +0200

    Changes to be committed:

--- src/components/ImgCell/ImgCell.tsx
+++ src/components/ImgCell/ImgCell.tsx
@@ -1,29 +1,18 @@
+import classes from './ImgCell.module.css';
+
 type MovieCardProps = {
   posterPath: string;
-  title: string;
+  title?: string;
 };
 
 export const ImgCell = ({ posterPath }: MovieCardProps) => {
   const imageUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
 
   return (
-    <div style={styles.card}>
-      <img src={imageUrl} alt={'title'} style={styles.image} />
+    <div className={classes.card}>
+      <img src={imageUrl} alt={'title'} className={classes.image} />
     </div>
   );
-};
-
-const styles = {
-  card: {
-    width: '200px',
-    borderRadius: '8px',
-    overflow: 'hidden',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    margin: 5,
-  },
-  image: {
-    width: '100%',
-  },
 };
 
 export default ImgCell;
